77a6d6409db09770e70cf986f837bc8d4e8b0304
GustavoSebastianR3 <gustavosebastian269@gmail.com>
2024-07-12 18:10:16 -0500

    commit

---
+++ standar-microservicio/standar-microservicio/src/main/java/com/standar/standar_microservicio/StandarMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.standar.standar_microservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandarMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandarMicroservicioApplication.class, args);
+	}
+
+}
---
+++ standar-microservicio/standar-microservicio/src/test/java/com/standar/standar_microservicio/StandarMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.standar.standar_microservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandarMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
